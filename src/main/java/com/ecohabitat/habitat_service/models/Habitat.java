98eece808f00f22c5c426f904828a450a9754ed1
peterParker79 <trypeterparker@gmail.com>
2025-05-26 09:45:24 +0200

    created model

---
+++ src/main/java/com/ecohabitat/habitat_service/models/Habitat.java
@@ -0,0 +1,24 @@
+package com.ecohabitat.habitat_service.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Habitat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String location;
+    private String type; //piso, chalet, mobileHome...
+    Long ownerId;
+}
