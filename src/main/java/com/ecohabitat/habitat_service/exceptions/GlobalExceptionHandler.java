229e8773a3460ad80b74bd352eaa853e96e2a854
peterParker79 <trypeterparker@gmail.com>
2025-05-26 12:13:11 +0200

    add Global Exception Handler

---
+++ src/main/java/com/ecohabitat/habitat_service/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.ecohabitat.habitat_service.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HabitatNotFoundException.class)
+    public ResponseEntity<String> handleHabitatNotFound(HabitatNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+    @ExceptionHandler(HabitatsNotFoundException.class)
+    public ResponseEntity<String> handleHabitatsNotFound(HabitatsNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+}
